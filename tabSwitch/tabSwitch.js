5b4991f58f07431910ab2229ae089a4ef4084fa5
huaziHear <huaziHear@163.com>
2015-10-08 19:12:35 +0800

    init tab

---
+++ tabSwitch/tabSwitch.js
@@ -0,0 +1,13 @@
+/**
+ * author: huazi
+ * date: 2015-10-08
+ * describe: 
+ * 		tab切换。
+ */
+var tabSwitch = function() {
+
+}
+
+tabSwitch.prototype.method_name = function(first_argument) {
+	// body...
+};
